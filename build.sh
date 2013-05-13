c592cfd2c1baf51ff878c64a6664ee4c66b2ec5f
mackenza <a.mackenzie@lancaster.ac.uk>
2013-05-13 12:22:11 +0100

    abstract after accident rm -Rf

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
- pandoc  --smart --normalize --latex-engine=xelatex  --template=template.latex --bibliography=references/refs.bib warwick_paper.rmd -o warwick_paper.pdf 
- # pandoc  --smart --normalize --latex-engine=xelatex   --bibliography=references/refs.bib warwick_paper.rmd -o warwick_paper.pdf 
- evince warwick_paper.pdf
+pandoc --smart --normalize --latex-engine=xelatex  --template=template.latex --bibliography=references/refs.bib warwick.rmd -o warwick.pdf
+
+evince warwick.pdf
